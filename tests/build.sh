73ca3e04c75d42f51fbfc41a0e2043a46e302d73
Prem chavhan <pchavhan@logiquad.com>
2023-11-17 19:37:44 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-rm -f /server-product/docker/docker-compose.yml
-cp -rf /server-product/docker/* ./
+rm -f ./server-product/docker/docker-compose.yaml
+
+mvn clean package
+
+cd ./server-product/target
+rm -rf docker
+cp -r ../docker docker
+buildVersion=`ls onedev-*.zip|sed -e 's/onedev-\(.*\).zip/\1/'`
+
+unzip onedev-$buildVersion.zip -d docker
+mv docker/onedev-$buildVersion docker/app
+
+cd ../../
+
+cp -rf /server-product/target/docker/* ./
 
 mv docker-compose-new.yml docker-compose.yml
+mv Dockerfile.server Dockerfile
 
-docker buildx build . -f Dockerfile.server --output type=docker,name=elestio4test/onedev-server:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/onedev-server:latest | docker load
